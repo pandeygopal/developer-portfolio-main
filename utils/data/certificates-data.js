97527866fadfdc14e3c6990e745a939acb0b7381
Gopal <145958775+pandeygopal@users.noreply.github.com>
2026-05-26 09:59:41 +0530

    added CSA

--- utils/data/certificates-data.js
+++ utils/data/certificates-data.js
@@ -1,28 +1,34 @@
 export const certificatesData = [
-    {
-      id: 1,
-      name: 'Microsoft Certified: Azure AI Fundamentals',
-      issuingBody: 'Microsoft',
-      issueDate: 'June 2023',
-      credentialLink: 'https://learn.microsoft.com/en-us/users/gopalpandey-1868/credentials/26d66d32b77f2383?ref=https%3A%2F%2Fwww.linkedin.com%2F',
-      image: '/image/MicrosoftAzure.png',
-    },
-    {
-      id: 2,
-      name: 'Nptel - Computer Networks and Internet Protocols',
-      issuingBody: 'Nptel',
-      issueDate: 'April 2025',
-      credentialLink: 'https://drive.google.com/file/d/1aEFS0vHg5_uB7cXO031ESJBnIzxq5HgO/view',
-      image: '/image/Nptel.png',
-    }
-    ,
-    {
-      id: 3,
-      name: 'Interoduction to cyber security',
-      issuingBody: 'Cisco',
-      issueDate: 'July 2024',
-      credentialLink: 'https://www.netacad.com/certificates?issuanceId=8ed9360a-2369-4d74-9aa4-79bfeafddfef',
-      image: '/image/cyber-1.png', // You need to add this image to your /public/certificates/ folder
-    }
-    // Add more certificates here
-  ]
+  {
+    id: 1,
+    name: 'Microsoft Certified: Azure AI Fundamentals',
+    issuingBody: 'Microsoft',
+    issueDate: 'June 2023',
+    credentialLink: 'https://learn.microsoft.com/en-us/users/gopalpandey-1868/credentials/26d66d32b77f2383?ref=https%3A%2F%2Fwww.linkedin.com%2F',
+    image: '/image/MicrosoftAzure.png',
+  },
+  {
+    id: 2,
+    name: 'Nptel - Computer Networks and Internet Protocols',
+    issuingBody: 'Nptel',
+    issueDate: 'April 2025',
+    credentialLink: 'https://drive.google.com/file/d/1aEFS0vHg5_uB7cXO031ESJBnIzxq5HgO/view',
+    image: '/image/Nptel.png',
+  },
+  {
+    id: 3,
+    name: 'Introduction to Cyber Security',
+    issuingBody: 'Cisco',
+    issueDate: 'July 2024',
+    credentialLink: 'https://www.netacad.com/certificates?issuanceId=8ed9360a-2369-4d74-9aa4-79bfeafddfef',
+    image: '/image/cyber-1.png',
+  },
+  {
+    id: 4,
+    name: 'ServiceNow Certified System Administrator (CSA)',
+    issuingBody: 'ServiceNow',
+    issueDate: '2025',
+    credentialLink: 'https://www.credly.com/badges/4181edf1-3148-4ef3-ac67-f177ac0951a6',
+    image: '/image/CSA.png',
+  }
+]
